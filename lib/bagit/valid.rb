7d8bfa563b14005f37f85e67f7dc7805f06b15b1
Hector Correa <hector@hectorcorrea.com>
2015-07-17 15:23:20 -0400

    Raise a sensible exception when the bag has a manifest with an unknown algorithm (instead of throwing ":unknown:Symbol" error)
    Conflicts:
    	bagit.gemspec
    	lib/bagit/valid.rb

--- lib/bagit/valid.rb
+++ lib/bagit/valid.rb
@@ -32,21 +32,21 @@ module BagIt
     def consistent?
       (manifest_files|tagmanifest_files).each do |mf|
         # get the algorithm implementation
-        readAlgo = /manifest-(.+).txt$/.match(File.basename(mf))[1]
-        algo = case readAlgo
-                 when /sha1/i
-                   Digest::SHA1
-                 when /md5/i
-                   Digest::MD5
-                 when /sha256/i
-                   Digest::SHA256
-                 when /sha384/i
-                   Digest::SHA384
-                 when /sha512/i
-                   Digest::SHA512
-                 else
-                   errors.add :consistency, "Algorithm #{readAlgo} is invalid or unsupported."
-                   return false
+        File.basename(mf) =~ /manifest-(.+).txt$/
+        manifest_type = $1
+        algo = case manifest_type
+               when /sha1/i
+                 Digest::SHA1
+               when /md5/i
+                 Digest::MD5
+               when /sha256/i
+                 Digest::SHA256
+               when /sha384/i
+                 Digest::SHA384
+               when /sha512/i
+                 Digest::SHA512
+               else
+                 raise ArgumentError.new("Algorithm #{manifest_type} is not supported.")
                end
         # Check every file in the manifest
         File.open(mf) do |io|
